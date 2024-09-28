80235c12ef33b3d44dbef6ab29a5d376870b88ed
Chandra Tanabuddi <test@test.com>
2024-09-27 20:30:17 -0600

    Text_to_PDF read data and displayed

--- Text_to_PDF.py
+++ Text_to_PDF.py
@@ -14,8 +14,16 @@ for filepath in filepaths:
 
      filename= (Path(filepath).stem).title()
 
-
+    # Adding the file name as header
      pdf.set_font(family="Times", style="B", size=12)
-     pdf.cell(w=50, h= 8,txt=filename)
+     pdf.cell(w=50, h= 8,txt=filename, ln=1)
+
+     #Adding the content of files using multi-cells
+     with open(filepath, 'r') as file:
+          content = file.read()
+     pdf.set_font(family= "Times", size=8)
+     # content = pd.read_csv
+     pdf.multi_cell(w=0, h=6, txt = content)
+
 
 pdf.output("Text_to_PDF/output.pdf")
